5afd5e9ab8b08d2ff861f1f77132af0337819ab2
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-09-09 12:19:22 +0100

    added jest.setup.js and created a sample test

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", {
+      name: /Welcome to Next\.js!/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});

7ef2afd095a0c1591cdb7a726a612a15acda885d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-10-02 13:01:18 +0100

    created components dir, added Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import { ChakraProvider, Container } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Layout from "../components/Layout";
 
 function PortfolioDashboardApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Container>
+      <Head>
+        <title>Portfolio Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
         <Component {...pageProps} />
-      </Container>
+      </Layout>
     </ChakraProvider>
   );
 }

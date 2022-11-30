f17e17d705b41d1b6b0a159b0fe6584a89260bea
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-11-30 23:05:58 +0000

    added main.tf, .gitignore for terraform setup

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-05bfd03d0709e3ecb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}

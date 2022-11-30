74faaa519926dca819883b3193d3c94cd2f22824
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-11-30 23:50:39 +0000

    added terraform cloud

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "syed-taqi-haider"
+    workspaces {
+      name = "portfolio-dashboard-prod-eu-west-2"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,11 +20,4 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-05bfd03d0709e3ecb"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+  description = "Value of the name tag of the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}

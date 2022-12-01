7254469e8b2d880b30ef0cffef83bdd97ef0380e
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-01 20:07:38 +0000

    added tf module : cloudfront-s3-cdn

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,22 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+# Creates S3 bucket to host static assets
+# and configures a Cloudfront CDN distribution
+module "cdn" {
+  source = "cloudposse/cloudfront-s3-cdn/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version = "x.x.x"
+
+  namespace         = "eg"
+  stage             = "prod"
+  name              = var.project_name
+  aliases           = [var.domain_name]
+  dns_alias_enabled = true
+  parent_zone_name  = "syedtaqihaider.co.uk"
+
+  deployment_principal_arns = {
+    (var.admin_iam_arn) = [""]
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,8 @@ variable "domain_name" {
   type        = string
   description = "The domain name for the website."
 }
+
+variable "admin_iam_arn" {
+  type        = string
+  description = "ARN of the Administrator."
+}

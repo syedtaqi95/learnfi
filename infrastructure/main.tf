669da8c5f506d48d47747984bdd8a9731660bb30
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-04 17:27:34 +0400

    renamed "terraform" directory to "infrastructure"

---
+++ infrastructure/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+    organization = "syed-taqi-haider"
+    workspaces {
+      name = "portfolio-dashboard-prod-eu-west-2"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}

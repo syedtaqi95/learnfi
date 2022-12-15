cf5d964d601dd566c0d83726c720b40c2f0194da
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-16 00:03:29 +0400

    added www domains to ssl_certificate

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -1,10 +1,14 @@
-# SSL Certificate
+# SSL Certificate for root website (non-www)
 resource "aws_acm_certificate" "ssl_certificate" {
-  provider = aws.acm_provider
+  provider          = aws.acm_provider
+  domain_name       = var.domain_name
+  validation_method = "DNS"
 
-  domain_name               = var.domain_name
-  subject_alternative_names = ["*.${var.domain_name}"]
-  validation_method         = "DNS"
+  subject_alternative_names = [
+    "*.${var.domain_name}",
+    "${var.subdomain_name}.${var.domain_name}",
+    "*.${var.subdomain_name}.${var.domain_name}"
+  ]
 
   lifecycle {
     create_before_destroy = true

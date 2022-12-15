24b57e01f90a28cf65721cd33815f14830f6bc26
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-15 16:56:43 +0400

    added www s3 bucket

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -64,3 +64,59 @@ resource "aws_s3_bucket_website_configuration" "root_bucket_website_config" {
   index_document { suffix = "index.html" }
   error_document { key = "index.html" }
 }
+
+#############################
+# www S3 bucket resources
+#############################
+
+# S3 bucket to redirect www requests to non-www.
+resource "aws_s3_bucket" "www_s3_bucket" {
+  bucket        = "www.${var.subdomain_name}.${var.domain_name}"
+  force_destroy = true
+}
+
+# Access Control List for www_S3_bucket
+# Set to private to allow access only through Cloudfront
+resource "aws_s3_bucket_acl" "www_s3_acl" {
+  bucket = aws_s3_bucket.www_s3_bucket.id
+  acl    = "private"
+}
+
+# S3 bucket policy
+resource "aws_s3_bucket_policy" "www_bucket_policy" {
+  bucket = aws_s3_bucket.www_s3_bucket.id
+  policy = data.aws_iam_policy_document.www_bucket_policy.json
+}
+
+# Policy document for www_bucket_policy to allow only private access through Cloudfront
+data "aws_iam_policy_document" "www_bucket_policy" {
+  statement {
+    sid = "AllowCloudFrontServicePrincipal"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      aws_s3_bucket.www_s3_bucket.arn,
+      "${aws_s3_bucket.www_s3_bucket.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.www_s3_distribution.arn]
+    }
+  }
+}
+
+# Website config for www_s3_bucket
+resource "aws_s3_bucket_website_configuration" "www_bucket_website_config" {
+  bucket = aws_s3_bucket.www_s3_bucket.id
+  redirect_all_requests_to {
+    host_name = "${var.subdomain_name}.${var.domain_name}"
+  }
+}

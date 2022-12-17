f27a535b5aa7ce33b7e4238ca7845a4486b3ad3d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-17 16:46:46 +0400

    created HomePage feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,10 @@
-import { Box, Container, Heading } from "@chakra-ui/react"
+import HomePage from "@/features/HomePage"
 
 function App() {
   return (
-    <Box>
-      <Heading>LearnFi</Heading>
-      <Container>
-        A web-based learning platform that helps beginners learn the basics of investing.
-        <br />
-        Learn how to build a portfolio, analyse assets and more.
-      </Container>
-    </Box>
+    <>
+      <HomePage />
+    </>
   )
 }
 
---
+++ frontend/src/features/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import { Box, Container, Heading } from "@chakra-ui/react"
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Heading>build-a-portfolio</Heading>
+      <Container>
+        An open-source web app that helps beginners learn the basics of investing.
+        <br />
+        Learn how to build a portfolio, analyse assets and more.
+      </Container>
+    </Box>
+  )
+}
+
+export default HomePage

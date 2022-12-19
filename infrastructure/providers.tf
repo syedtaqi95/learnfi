bb405194280069d86581fb647b3d0e7cd78e8928
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-19 12:53:42 +0400

    renamed project to LearnFi

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -18,10 +18,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-2"
+  region     = "eu-west-2"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 provider "aws" {
-  alias  = "acm_provider"
-  region = "us-east-1"
+  alias      = "acm_provider"
+  region     = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }

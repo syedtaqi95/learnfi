68b04bec798d2c0a3f111167114312a748cf644e
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-20 21:07:25 +0400

    centred Hero

--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Heading, VStack } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
-    <>
-      <Heading>LearnFi</Heading>
+    <VStack mt={36}>
+      <Heading as="h1" fontSize={{ base: "5xl", md: "8xl" }}>
+        LearnFi
+      </Heading>
       <Container>
         A web-based learning platform that helps beginners learn the basics of
         investing.
@@ -173,7 +175,7 @@ const Hero = () => {
         velit ducimus sit dicta repellat fuga optio quo debitis quibusdam nobis
         fugit! Tenetur, adipisci.
       </Container>
-    </>
+    </VStack>
   );
 };
 

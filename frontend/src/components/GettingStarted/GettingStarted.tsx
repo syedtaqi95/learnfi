448c87eec3a7e34b7a19fe29dc1a31cf3104b46c
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-23 15:11:17 +0400

    added Cards to GettingStarted

--- frontend/src/components/GettingStarted/GettingStarted.tsx
+++ frontend/src/components/GettingStarted/GettingStarted.tsx
@@ -1,5 +1,6 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Flex, Card, CardHeader } from "@chakra-ui/react";
 import HomePageSection from "@/components/HomePageSection";
+import TextBox from "@/components/TextBox";
 
 const GettingStarted = () => {
   return (
@@ -7,6 +8,34 @@ const GettingStarted = () => {
       <Heading as="h2" textStyle={"section_heading"}>
         Getting Started
       </Heading>
+
+      {/* Cards */}
+      <Flex
+        pt={"40px"}
+        w="100%"
+        direction={{ base: "column", lg: "row" }}
+        justifyContent={"center"}
+        gap={4}
+        gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      >
+        <Card>
+          <CardHeader>
+            <Heading size="md">
+              Step 1
+            </Heading>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Step 2</Heading>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Step 3</Heading>
+          </CardHeader>
+        </Card>
+      </Flex>
     </HomePageSection>
   );
 };

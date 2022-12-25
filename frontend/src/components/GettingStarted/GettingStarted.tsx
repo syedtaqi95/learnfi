bf60e51b862400700494dba6b8857b8ce2a4ea8d
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-25 16:13:36 +0400

    updated GettingStarted section

--- frontend/src/components/GettingStarted/GettingStarted.tsx
+++ frontend/src/components/GettingStarted/GettingStarted.tsx
@@ -1,8 +1,25 @@
-import { Heading, Flex, Card, CardHeader } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Card,
+  SimpleGrid,
+  CardBody,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import HomePageSection from "@/components/HomePageSection";
 import TextBox from "@/components/TextBox";
 
 const GettingStarted = () => {
+  const cardStyles = {
+    boxShadow: `0 0 14px var(--chakra-colors-${useColorModeValue(
+      "gray-500",
+      "green-400"
+    )})`,
+    fontSize: { base: "20px", md: "28px" },
+  };
+
+  const textBoxStyles = { textAlign: "start", textStyle: "paragraph" };
+
   return (
     <HomePageSection variant="section">
       <Heading as="h2" textStyle={"section_heading"}>
@@ -10,32 +27,38 @@ const GettingStarted = () => {
       </Heading>
 
       {/* Cards */}
-      <Flex
-        pt={"40px"}
-        w="100%"
-        direction={{ base: "column", lg: "row" }}
-        justifyContent={"center"}
-        gap={4}
-        gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+      <SimpleGrid
+        pt={{ base: 2, md: 8 }}
+        spacing={8}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(3, 1fr)" }}
+        maxW="100%"
       >
-        <Card>
-          <CardHeader>
-            <Heading size="md">
-              Step 1
-            </Heading>
-          </CardHeader>
+        <Card {...cardStyles}>
+          <CardBody>
+            <Text>1 Create an account</Text>
+            <TextBox styles={textBoxStyles}>
+              Sign up using a username and password or your email address
+            </TextBox>
+          </CardBody>
         </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Step 2</Heading>
-          </CardHeader>
+        <Card {...cardStyles}>
+          <CardBody>
+            <Text>2 Explore the topics</Text>
+            <TextBox styles={textBoxStyles}>
+              Work through the sections at your own pace
+            </TextBox>
+          </CardBody>
         </Card>
-        <Card>
-          <CardHeader>
-            <Heading size="md">Step 3</Heading>
-          </CardHeader>
+        <Card {...cardStyles}>
+          <CardBody>
+            <Text>3 Apply your learning</Text>
+            <TextBox styles={textBoxStyles}>
+              Use our platform to apply what you have learned to your investing
+              endeavours
+            </TextBox>
+          </CardBody>
         </Card>
-      </Flex>
+      </SimpleGrid>
     </HomePageSection>
   );
 };

b53f5bdc1f3cfc5ee30fdc673257111a0cf17cb9
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-26 16:18:21 +0400

    added text to Contact

--- frontend/src/components/Contact/Contact.tsx
+++ frontend/src/components/Contact/Contact.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import HomePageSection from "@/components/HomePageSection";
 import { Heading } from "@chakra-ui/react";
+import TextBox from "@/components/TextBox";
 const Contact = () => {
   return (
     <HomePageSection variant="section">
       <Heading as="h2" textStyle={"section_heading"}>
-        Get in touch!
+        ✉️ Get in touch
       </Heading>
+
+      <TextBox>
+        Thank you for visiting our website! We are always happy to hear from our
+        visitors and welcome any feedback, questions, or comments you may have.
+        Please don't hesitate to reach out. You can contact us using one of the
+        options below.
+      </TextBox>
     </HomePageSection>
   );
 };

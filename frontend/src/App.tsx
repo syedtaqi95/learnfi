ed0d4cefa384a15d79e6bad9cfeaca4245fcdda6
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-26 22:42:01 +0400

    added default route and ErrorPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-import HomePage from "@/features/HomePage";
+import HomePage from "@/routes/HomePage";
 import Navbar from "@/components/Navbar";
+import ErrorPage from "@/routes/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Client-side router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <HomePage />
+      </>
+    ),
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <HomePage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/routes/HomePage/HomePage.tsx
+++ frontend/src/routes/HomePage/HomePage.tsx
@@ -2,20 +2,24 @@ import Hero from "@/components/Hero";
 import Syllabus from "@/components/Syllabus";
 import GettingStarted from "@/components/GettingStarted";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import { Container, Divider } from "@chakra-ui/react";
 import Contact from "@/components/Contact";
 
 const HomePage = () => {
   return (
-    <Container as="main" maxW={{ base: "90vw", lg: "80vw" }}>
-      <Hero />
-      <Divider />
-      <Syllabus />
-      <GettingStarted />
-      <Contact />
-      <Divider />
-      <Footer />
-    </Container>
+    <>
+      <Navbar />
+      <Container as="main" maxW={{ base: "90vw", lg: "80vw" }}>
+        <Hero />
+        <Divider />
+        <Syllabus />
+        <GettingStarted />
+        <Contact />
+        <Divider />
+        <Footer />
+      </Container>
+    </>
   );
 };
 

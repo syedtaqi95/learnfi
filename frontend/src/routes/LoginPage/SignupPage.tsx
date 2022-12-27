e8d607605372e203f0e2d9a71a7859b2a13edd5f
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-27 18:16:30 +0400

    added email, password and submit button to SignupPage

--- frontend/src/routes/LoginPage/SignupPage.tsx
+++ frontend/src/routes/LoginPage/SignupPage.tsx
@@ -1,10 +1,66 @@
 import Navbar from "@/components/Navbar";
+import { Form } from "react-router-dom";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  useColorModeValue,
+  VStack,
+} from "@chakra-ui/react";
 
 const LoginPage = () => {
+  const formControlStyles = {
+    mb: "16px",
+  };
+
   return (
     <>
       <Navbar />
-      <div>LoginPage</div>
+      <Container
+        as="main"
+        textAlign="center"
+        maxW={{ base: "90vw", lg: "80vw" }}
+      >
+        <VStack>
+          <Heading as="h2" mt="100px" mb="24px">
+            Sign up
+          </Heading>
+
+          <Form
+            style={{ width: "100%", maxWidth: "300px" }}
+            method="post"
+            action="/events"
+          >
+            {/* Email address */}
+            <FormControl {...formControlStyles}>
+              <Input
+                borderColor={useColorModeValue("gray.500", "gray.400")}
+                type="email"
+                name="email"
+                placeholder="Email"
+              />
+            </FormControl>
+
+            {/* Password */}
+            <FormControl {...formControlStyles}>
+              <Input
+                borderColor={useColorModeValue("gray.500", "gray.400")}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+            </FormControl>
+
+            {/* Submit button */}
+            <Button  loadingText="Submitting...">
+              Submit
+            </Button>
+          </Form>
+        </VStack>
+      </Container>
     </>
   );
 };

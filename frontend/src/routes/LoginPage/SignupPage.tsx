ad350815081c7183af9ac897482255bb69584a55
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-29 17:27:47 +0400

    edited password input and added Google login button

--- frontend/src/routes/LoginPage/SignupPage.tsx
+++ frontend/src/routes/LoginPage/SignupPage.tsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import Navbar from "@/components/Navbar";
 import { Form } from "react-router-dom";
 import {
   Button,
   Container,
+  Text,
   FormControl,
-  FormLabel,
   Heading,
   Input,
+  InputGroup,
+  InputRightElement,
   useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
+  // Password show/hide button
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePasswordButton = () => setShowPassword(!showPassword);
+
+  // Loading state
+  const [isLoading, setLoading] = useState(false);
+
+  // Form field styles
   const formControlStyles = {
     mb: "16px",
   };
@@ -19,21 +31,13 @@ const LoginPage = () => {
   return (
     <>
       <Navbar />
-      <Container
-        as="main"
-        textAlign="center"
-        maxW={{ base: "90vw", lg: "80vw" }}
-      >
+      <Container as="main" centerContent maxW={{ base: "90vw", lg: "80vw" }}>
         <VStack>
           <Heading as="h2" mt={{ base: "100px", md: "15vh" }} mb="24px">
             Sign up
           </Heading>
 
-          <Form
-            style={{ width: "100%", maxWidth: "300px" }}
-            method="post"
-            action="/events"
-          >
+          <Form method="post" action="/events">
             {/* Email address */}
             <FormControl {...formControlStyles}>
               <Input
@@ -47,27 +51,50 @@ const LoginPage = () => {
 
             {/* Password */}
             <FormControl {...formControlStyles}>
-              <Input
-                isRequired
-                borderColor={useColorModeValue("gray.500", "gray.400")}
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
+              <InputGroup>
+                <Input
+                  pr="74px"
+                  isRequired
+                  borderColor={useColorModeValue("gray.500", "gray.400")}
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  placeholder="Password"
+                />
+                <InputRightElement width="72px">
+                  <Button
+                    w="95%"
+                    h="90%"
+                    borderLeftRadius="0"
+                    borderRightRadius="4px"
+                    bg={useColorModeValue("gray.200", "gray.700")}
+                    _hover={{ bg: useColorModeValue("gray.300", "gray.600") }}
+                    _active={{ bg: useColorModeValue("gray.400", "gray.500") }}
+                    fontSize={"14px"}
+                    onClick={handlePasswordButton}
+                  >
+                    {showPassword ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
             </FormControl>
 
             {/* Submit button */}
             <Button
+              variant="smallButton"
               type="submit"
               w="100%"
-              bg={useColorModeValue("gray.200", "gray.700")}
-              _hover={{bg: useColorModeValue("gray.300", "gray.600")}}
               loadingText="Submitting..."
-              isDisabled={true}
+              isLoading={isLoading}
             >
               Submit
             </Button>
           </Form>
+
+          <Text>or</Text>
+
+          <Button leftIcon={<FcGoogle />} variant="smallButton" w="100%">
+            Continue with Google
+          </Button>
         </VStack>
       </Container>
     </>

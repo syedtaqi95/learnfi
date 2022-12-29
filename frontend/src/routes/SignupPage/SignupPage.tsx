9245400c9598edcff5e498a49e27172a21c3a3b7
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-29 22:32:09 +0400

    added email and password validation to form

--- frontend/src/routes/SignupPage/SignupPage.tsx
+++ frontend/src/routes/SignupPage/SignupPage.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
-import { Button, Container, Text, Heading, VStack } from "@chakra-ui/react";
-import { FcGoogle } from "react-icons/fc";
+import { Container, Heading, VStack } from "@chakra-ui/react";
 import SignupForm from "@/components/SignupForm";
 
 const SignupPage = () => {
@@ -14,12 +13,6 @@ const SignupPage = () => {
           </Heading>
 
           <SignupForm />
-
-          <Text>or</Text>
-
-          <Button leftIcon={<FcGoogle />} variant="smallButton" w="100%">
-            Continue with Google
-          </Button>
         </VStack>
       </Container>
     </>

3a6297be0f5f983bad90f392d06c6d4c75ce8628
Syed Taqi Haider <sh13226@my.bristol.ac.uk>
2022-12-29 22:32:09 +0400

    added email and password validation to form

--- frontend/src/components/SignupForm/SignupForm.tsx
+++ frontend/src/components/SignupForm/SignupForm.tsx
@@ -8,17 +8,20 @@ import {
   InputGroup,
   InputRightElement,
   useColorModeValue,
+  Text,
 } from "@chakra-ui/react";
 import { ActionFunction, redirect } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
-// Action triggered when user submits form
+// Action triggered when on submitting form
 export const action: ActionFunction = async ({ request }) => {
+  // Retrieve form fields
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
   const errors: { email?: string; password?: string } = {};
 
-  // validate the fields
+  // Validate the fields
   if (typeof email !== "string" || !email.includes("@")) {
     errors.email = "Enter a valid email address";
   }
@@ -27,11 +30,12 @@ export const action: ActionFunction = async ({ request }) => {
     errors.password = "Password must be greater than 6 characters";
   }
 
-  // return data if we have errors
+  // Return data if we have errors
   if (Object.keys(errors).length) {
     return errors;
   }
 
+  // Redirect to home page
   return redirect("/");
 };
 
@@ -48,55 +52,67 @@ const SignupForm = () => {
   };
 
   return (
-    <Form method="post">
-      {/* Email address */}
-      <FormControl {...formControlStyles}>
-        <Input
-          autoFocus
-          isRequired
-          borderColor={useColorModeValue("gray.500", "gray.400")}
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        {errors?.email ? <FormHelperText>{errors.email}</FormHelperText> : null}
-      </FormControl>
-
-      {/* Password */}
-      <FormControl {...formControlStyles}>
-        <InputGroup>
+    <>
+      <Form method="post">
+        {/* Email address */}
+        <FormControl {...formControlStyles}>
           <Input
-            pr="74px"
+            autoFocus
             isRequired
             borderColor={useColorModeValue("gray.500", "gray.400")}
-            type={showPassword ? "text" : "password"}
-            name="password"
-            placeholder="Password"
+            type="email"
+            name="email"
+            placeholder="Email"
           />
-          <InputRightElement width="72px">
-            <Button
-              w="95%"
-              h="90%"
-              borderLeftRadius="0"
-              borderRightRadius="4px"
-              bg={useColorModeValue("gray.200", "gray.700")}
-              _hover={{ bg: useColorModeValue("gray.300", "gray.600") }}
-              _active={{ bg: useColorModeValue("gray.400", "gray.500") }}
-              fontSize={"14px"}
-              onClick={handlePasswordButton}
-            >
-              {showPassword ? "Hide" : "Show"}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        {errors?.password && <FormHelperText>{errors.password}</FormHelperText>}
-      </FormControl>
+          {errors?.email ? (
+            <FormHelperText>{errors.email}</FormHelperText>
+          ) : null}
+        </FormControl>
 
-      {/* Submit button */}
-      <Button variant="smallButton" type="submit" w="100%">
-        Submit
+        {/* Password */}
+        <FormControl {...formControlStyles}>
+          <InputGroup>
+            <Input
+              pr="74px"
+              isRequired
+              borderColor={useColorModeValue("gray.500", "gray.400")}
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+            />
+            <InputRightElement width="72px">
+              <Button
+                w="95%"
+                h="90%"
+                borderLeftRadius="0"
+                borderRightRadius="4px"
+                bg={useColorModeValue("gray.200", "gray.700")}
+                _hover={{ bg: useColorModeValue("gray.300", "gray.600") }}
+                _active={{ bg: useColorModeValue("gray.400", "gray.500") }}
+                fontSize={"14px"}
+                onClick={handlePasswordButton}
+              >
+                {showPassword ? "Hide" : "Show"}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {errors?.password && (
+            <FormHelperText color="red">{errors.password}</FormHelperText>
+          )}
+        </FormControl>
+
+        {/* Submit button */}
+        <Button variant="smallButton" type="submit" w="100%">
+          Submit
+        </Button>
+      </Form>
+
+      <Text>or</Text>
+
+      <Button leftIcon={<FcGoogle />} variant="smallButton" w="100%">
+        Continue with Google
       </Button>
-    </Form>
+    </>
   );
 };
 
